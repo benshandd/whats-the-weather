55cf3c6c4da082eb2e6eee36fa5071e2e31fb7df
Ben Shand <benjaminshand101@gmail.com>
2023-12-15 19:55:15 +1300

    Integrated Tailwind.css and add favouriting functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import WeatherApp from "./components/WeatherApp";
 
@@ -6,7 +5,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <WeatherApp/>
       </header>
     </div>
--- src/components/WeatherApp.js
+++ src/components/WeatherApp.js
@@ -1,45 +1,122 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import "./WeatherApp.css";
 
 const WeatherApp = () => {
-  const [city, setCity] = useState('');
-  const [weatherData, setWeatherData] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState('');
+  const [search, setSearch] = useState("");
+  const [weather, setWeather] = useState(null);
+  const [favourites, setFavourites] = useState([]);
+  const [currentFavouriteIndex, setCurrentFavouriteIndex] = useState(0);
 
-  const apiKey = 'YOUR_OPENWEATHERMAP_API_KEY';
+  const api = {
+    key: "b616c6aa3c56788fcbd0e5aa0dbc47fe",
+    base: "https://api.openweathermap.org/data/2.5/",
+  };
 
-  const fetchWeatherData = async () => {
-    setLoading(true);
-    setError('');
+  const searchPressed = () => {
+    fetch(api.base + "weather?q=" + search + "&units=metric&APPID=" + api.key)
+      .then(function (res) {
+        return res.json();
+      })
+      .then(function (result) {
+        setWeather(result);
+      });
+  };
 
-    try {
-      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`);
-      const data = await response.json();
-
-      if (response.ok) {
-        setWeatherData(data);
-      } else {
-        setError(data.message || 'Unable to fetch weather data');
-      }
-    } catch (error) {
-      setError('An error occurred while fetching weather data');
-    } finally {
-      setLoading(false);
+  const addToFavourites = () => {
+    if (
+      weather &&
+      !favourites.some(function (fav) {
+        return fav.id === weather.id;
+      })
+    ) {
+      setFavourites(favourites.concat([weather]));
+      setCurrentFavouriteIndex(favourites.length); // Set current index to the newly added favourite
     }
   };
 
+  const removeFavourite = (favouriteId) => {
+    // Use the filter method to create a new array excluding the favourite with the specified id
+    const updatedFavourites = favourites.filter(function (fav) {
+      return fav.id !== favouriteId;
+    });
+
+    // Update the 'favourites' state with the new array excluding the removed favourite
+    setFavourites(updatedFavourites);
+
+    // Adjust the current index when a favourite is removed
+    if (currentFavouriteIndex >= updatedFavourites.length) {
+      setCurrentFavouriteIndex(Math.max(0, updatedFavourites.length - 1));
+    }
+  };
+
+  const navigateFavourite = (direction) => {
+    const newIndex =
+      (currentFavouriteIndex + direction + favourites.length) % favourites.length;
+    setCurrentFavouriteIndex(newIndex);
+  };
+
   return (
-    <div>
-      <h1>Weather App</h1>
-      <div>
-        <label htmlFor="cityInput">Enter City:</label>
-        <input
-          type="text"
-          id="cityInput"
-          value={city}
-          onChange={(e) => setCity(e.target.value)}
-        />
-      </div>
+    <div className="App">
+      <header className="App-header">
+        <h1>Weather App☁️</h1>
+        <div>
+          <input
+            type="text"
+            placeholder="Enter location..."
+            onChange={function (e) {
+              setSearch(e.target.value);
+            }}
+          />
+          <button onClick={searchPressed}> Search🔎 </button>
+          <button onClick={addToFavourites}> Add to Favourites🌟 </button>
+        </div>
+
+        {weather && typeof weather.main !== "undefined" ? (
+          <div>
+            <p>{weather.name}</p>
+            <p> {weather.main.temp} C°</p>
+            <p>{weather.weather[0].main}</p>
+            <p>{weather.weather[0].description}</p>
+          </div>
+        ) : (
+          ""
+        )}
+
+        {favourites.length > 0 && (
+          <div>
+            <h2>Favourites🌟</h2>
+            <div>
+              <button
+                onClick={function () {
+                  navigateFavourite(-1);
+                }}
+              >
+                ⬅️ Prev
+              </button>
+              <button
+                onClick={function () {
+                  navigateFavourite(1);
+                }}
+              >
+                Next ➡️
+              </button>
+            </div>
+            <div key={favourites[currentFavouriteIndex].id}>
+              <p>{favourites[currentFavouriteIndex].name}</p>
+              <p> {favourites[currentFavouriteIndex].main.temp} C°</p>
+              <p>{favourites[currentFavouriteIndex].weather[0].main}</p>
+              <p>{favourites[currentFavouriteIndex].weather[0].description}</p>
+              <button
+                onClick={function () {
+                  removeFavourite(favourites[currentFavouriteIndex].id);
+                }}
+              >
+                Remove from Favourites
+              </button>
+            </div>
+          </div>
+        )}
+      </header>
     </div>
   );
 };
